79e34c825a38f77fde9731b9d5c64351f8769a0f
Maman mary2 <azzizasolmemvictoria@gmail.com>
2020-09-29 23:18:23 +0300

    --- Auto-Git Commit ---

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $(".contact-form").show();
+    // $(".contact-form").validate({
+    //     rules: {
+    //         firstname: 'required',
+    //         lastname: 'required',
+    //         subject: 'required',
+    //         message: 'required',
+    //         email: {
+    //             required: true,
+    //             email: true
+    //         }
+    //     }
+    // });
+});
