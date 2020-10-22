584d820f7d185d3bf7874fa53b64d8c8310d14b9
Maman mary2 <azzizasolmemvictoria@gmail.com>
2020-10-23 00:56:13 +0300

    --- Auto-Git Commit ---

--- dashboard/js/validation.js
+++ dashboard/js/validation.js
@@ -1,20 +1,15 @@
 // CompanyDetails INPUT VALIDATION
 function companyDetailsValidation() {
-    var firstName = document.forms["companyDetailsForm"]["firstName"].value;
-    var lastName  = document.forms["companyDetailsForm"]["lastName"].value;
+    var firstName = document.forms["companyDetailsForm"]["Name"].value;
     var email     = document.forms["companyDetailsForm"]["email"].value;
     var telephone   = document.forms["companyDetailsForm"]["telephone"].value;
-    var adress   = document.forms["companyDetailsForm"]["adress"].value;
+    var adress   = document.forms["ccompanyDetailsForm"]["adress"].value;
     var costperkgwatt   = document.forms["companyDetailsForm"]["costperkgwatt"].value;
-    if(firstName =='') {
-        alert( "First Name is required." );
+    if(Name =='') {
+        alert( " Name is required." );
         document.companyDetailsForm.firstName.focus() ;
         return false;
-    } else if(lastName=='') {
-         alert("Last Name is required.");
-       document.companyDetailsForm.lastName.focus() ;
-        return false;
-    } else if(email == '') {
+    }  else if(email == '') {
         alert("Email is required.");
       document.companyDetailsForm.email.focus() ;
        return false;
@@ -36,31 +31,31 @@ function companyDetailsValidation() {
 }
 
 // PROFILE INPUT VALIDATION
-function ProfileValidation() {
-    var firstName = document.forms["ProfileForm"]["firstName"].value;
-    var lastName  = document.forms["ProfileForm"]["lastName"].value;
-    var email     = document.forms["ProfileForm"]["email"].value;
-    var telephone   = document.forms["ProfileForm"]["telephone"].value;
-    var adress   = document.forms["ProfileForm"]["adress"].value;
+function profileValidation() {
+    var firstName = document.forms["profileForm"]["firstName"].value;
+    var lastName  = document.forms["profileForm"]["lastName"].value;
+    var userEmail     = document.forms["profileForm"]["userEmail"].value;
+    var phoneNumber   = document.forms["profileForm"]["phoneNumber"].value;
+    var userAddress   = document.forms["profileForm"]["userAddress"].value;
     if(firstName =='') {
         alert( "First Name is required." );
-        document.Profile.firstName.focus() ;
+        document.profileForm.firstName.focus() ;
         return false;
     } else if(lastName=='') {
          alert("Last Name is required.");
-       document.ProfileForm.lastName.focus() ;
+       document.profileForm.lastName.focus() ;
         return false;
-    } else if(email == '') {
+    } else if(userEmail == '') {
         alert("Email is required.");
-      document.ProfileForm.email.focus() ;
+      document.profileForm.userEmail.focus() ;
        return false;
-   } else if(telephone == '') {
-        alert("telephone is required.");
-      document.ProfilesForm.telephone.focus() ;
+   } else if(isNaN(phoneNumber)) {
+        alert("Telephone is required.");
+      document.profileForm.phoneNumber.focus() ;
        return false;
-   } else if(adress == '') {
-       alert("adress is required.");
-       document.ProfileForm.adress.focus() ;
+   } else if(userAddress == '') {
+       alert("Adress is required.");
+       document.profileForm.userAddress.focus() ;
        return false;
     }
 }
