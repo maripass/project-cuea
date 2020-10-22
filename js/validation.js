6c66d2daf1bc13acc46b3b6995ca6fa0b3d39698
Maman mary2 <azzizasolmemvictoria@gmail.com>
2020-10-22 22:55:42 +0300

    --- Auto-Git Commit ---

---
+++ js/validation.js
@@ -0,0 +1,42 @@
+// LOGIN INPUT VALIDATION
+function loginValidation() {
+    var userEmail = document.forms["loginForm"]["userEmail"].value;
+    var userPassword = document.forms["loginForm"]["userPassword"].value;
+    var emailFilter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if(userEmail =='') {
+        alert( "Email is required." );
+        document.loginForm.userEmail.focus() ;
+        return false;
+    } else if(userPassword=='') {
+        alert("Password is required.");
+        document.loginForm.userPassword.focus() ;
+        return false;
+    } else if(!emailFilter.test(userEmail)){
+        alert( "Enter valid email." );
+        document.loginForm.userEmail.focus() ;
+        return false;
+    }
+}
+
+// CONTACT INPUT VALIDATION
+function contactValidation() {
+
+    var firstName = document.forms["contactForm"]["firstName"].value;
+
+    // var userPassword = document.forms["loginForm"]["userPassword"].value;
+    // var emailFilter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if(firstName =='') {
+        alert( "First Name is required." );
+        document.contactForm.firstName.focus() ;
+        return false;
+    } 
+    // else if(userPassword=='') {
+    //     alert("Password is required.");
+    //     document.loginForm.userPassword.focus() ;
+    //     return false;
+    // } else if(!emailFilter.test(userEmail)){
+    //     alert( "Enter valid email." );
+    //     document.loginForm.userEmail.focus() ;
+    //     return false;
+    // }
+}
