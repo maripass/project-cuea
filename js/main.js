4ea2697341edab6b3f54c8337aa325d707c839a9
Maman mary2 <azzizasolmemvictoria@gmail.com>
2020-10-22 22:25:33 +0300

    --- Auto-Git Commit ---

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
-$(document).ready(function() {
-    // $(".contact-form").show();
-    $(".contact-form").validate({
-        rules: {
-            firstName: 'required',
-            lastName: 'required',
-            subject: 'required',
-            message: 'required',
-            email: {
-                required: true,
-                email: true
-            }
-        }
-    });
-});
+// LOGIN INPUT VALIDATION
+function loginValidation() {
+    var userEmail = document.forms["loginForm"]["userEmail"].value;
+    var userPassword = document.forms["loginForm"]["userPassword"].value;
+    var emailFilter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if(userEmail =='') {
+        alert( "Email is required." );
+        document.loginForm.userEmail.focus() ;
+        return false;
+    } else if(userPassword=='') {
+        alert("Password is required.");
+        document.loginForm.userPassword.focus() ;
+        return false;
+    } else if(!emailFilter.test(userEmail)){
+        alert( "Enter valid email." );
+        document.loginForm.userEmail.focus() ;
+        return false;
+    }
+}
