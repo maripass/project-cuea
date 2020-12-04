f4ba7af05d00b464e48b2834b776834cc39998f4
Maman mary2 <azzizasolmemvictoria@gmail.com>
2020-12-04 18:33:17 +0300

    --- Auto-Git Commit ---

--- profile.php
+++ profile.php
@@ -108,12 +108,7 @@
             </form>
         </div>
     </section>
-
-    <script
-        src="https://code.jquery.com/jquery-3.4.1.min.js"
-        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-        crossorigin="anonymous">
-    </script>   
+  
     <script src="js/dashboard-validation"></script>
 
 </body>
