19502293a54faef531e2ed584eed776b00041732
Maman mary2 <azzizasolmemvictoria@gmail.com>
2020-12-12 21:45:55 +0300

    --- Auto-Git Commit ---

--- consumption.php
+++ consumption.php
@@ -32,10 +32,21 @@
     <section>
         <div style="border:1px solid red; width:50%;height:100px;margin-left:25%;margin-top:10%;">
             <div style="text-align:center;margin-top:30px;font-size:40px; font-weight:bold;">
-                0000100
+               <div id="token">0000100</div> 
             </div>
         </div>
     </section>
+    <script>
+        setInterval(() => {
+            incrementToken();
+            // var consumption=getElementById('consumption').innerHTML ="1";
+        }, (3000));
+        function incrementToken() {
+            
+            let x++;
+            console.log(x);
+        }
+    </script>
 </body>
 
 </html>
