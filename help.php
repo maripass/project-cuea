9ef2639f8e77403627e1326646857d1456f34e35
Maman mary2 <azzizasolmemvictoria@gmail.com>
2020-12-13 12:19:35 +0300

    --- Auto-Git Commit ---

--- help.php
+++ help.php
@@ -30,50 +30,47 @@
         
         
         <div style="width: 50%; margin: auto;">
-        <?php 
-            $userId=$_SESSION['userId'];
-            $query="SELECT * FROM help WHERE userId='$userId'";
-            $result=mysqli_query($con, $query);
-            if(mysqli_num_rows($result) > 0){
-                while($row= $result->fetch_assoc()) {
-                    ?>
-                    <div style="float: right; right: 0px; background-color: #3274d6; width: 70%; padding: 10px; margin: 10px -25px 0 0;">
-                        <div style="margin-bottom: 10px; font-size: 20px;">Me</div>
-                        <div>
-                        <?php echo $row['message'] ?>
+            <?php 
+                $userId=$_SESSION['userId'];
+                $query="SELECT * FROM help WHERE userId='$userId'";
+                $result=mysqli_query($con, $query);
+                if(mysqli_num_rows($result) > 0){
+                    while($row= $result->fetch_assoc()) {
+                        ?>
+                        <div style="float: right; right: 0px; background-color: #3274d6; width: 70%; padding: 10px; margin: 10px -25px 0 0;">
+                            <div style="margin-bottom: 10px; font-size: 20px;">Me</div>
+                            <div style="margin-bottom: 10px; font-size: 15px;"><?php echo date('M d Y',strtotime($row['createdAt'])) ?></div>
+                            <div>
+                            <?php echo $row['message'] ?>
+                            </div>
                         </div>
-                    </div>
-                <?php
+                    <?php
+                    }
                 }
-            }
 
-            $queryAdmin="SELECT * FROM helpresponse WHERE userId='$userId'";
-            $resultAdmin=mysqli_query($con, $queryAdmin);
-            if(mysqli_num_rows($resultAdmin) > 0){
-                while($rowAdmin= $resultAdmin->fetch_assoc()) {
-                    ?>
-                    <div style="float: left; left: 0px; background-color: #dcdcdc; width: 70%; padding: 10px; margin: 10px 0 70 0px;">
-                        <div style="margin-bottom: 10px; font-size: 20px;">Admin</div>
-                        <div>
-                        <?php echo $rowAdmin['message'] ?>
+                $queryAdmin="SELECT * FROM helpresponse WHERE userId='$userId'";
+                $resultAdmin=mysqli_query($con, $queryAdmin);
+                if(mysqli_num_rows($resultAdmin) > 0){
+                    while($rowAdmin= $resultAdmin->fetch_assoc()) {
+                        ?>
+                        <div style="float: left; left: 0px; background-color: #dcdcdc; width: 70%; padding: 10px 10px 100px 10px; margin: 10px 0 0 0px;">
+                            <div style="margin-bottom: 10px; font-size: 20px;">Admin</div>
+                            <div style="margin-bottom: 10px; font-size: 15px;"><?php echo date('M d Y',strtotime($rowAdmin['createdAt'])) ?></div>
+                            <div>
+                            <?php echo $rowAdmin['message'] ?>
+                            </div>
                         </div>
-                    </div>
-
-                    
-                <?php
+                    <?php
+                    }
                 }
-            }
-        
-
             ?>
             
+        <!-- <div style="padding-top:100px;"></div> -->
         </div>
+
 
         <div style="width: 50%; position: fixed; bottom: 0px; margin-left: 25%;">
             <form name="loginForm" method="POST" onsubmit="return loginValidation()">
-            
-        
-        
                 <div>
                     <textarea name="message" id="message" style="height: 100px;"></textarea>
                 </div>
