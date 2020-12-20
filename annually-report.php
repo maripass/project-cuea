3c7c0e25b24f815993f37cfb632aa05627bb325e
Maman mary2 <azzizasolmemvictoria@gmail.com>
2020-12-20 11:49:46 +0300

    --- Auto-Git Commit ---

--- annually-report.php
+++ annually-report.php
@@ -3,9 +3,17 @@
 	
 	if (!isset($_SESSION['userId'])) {
 		header('location: login.php');
-  }
+    }
+    $jsonArray = array();
+	$theYear   = date("Y");
+	if (isset($_POST['filterByYear'])) {
+        $yearInput = mysqli_real_escape_string($con, $_POST['yearInput']);
+        echo $_POST['yearInput'];
+		$theYear = $yearInput;
+	}
   
-  ?>
+?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 <head>
@@ -29,47 +37,47 @@
                 Pick Year
         </button>
 
-        <form id="filter" style="margin-top:15px; float:right;right:0px;margin-right:30px;">
-          <input type="number" style="padding:10px; width:100%" max="2020" min="2016"placeholder="YYYY">
-          <input type="submit" value="Filter">
+        <form id="filter" style="margin-top:15px; float:right;right:0px;margin-right:30px;" method="POST">
+          <input type="number" name="yearInput" id="yearInput" style="padding:10px; width:100%" max="2020" min="2016"placeholder="YYYY" value="<?php echo $yearInput ?>">
+          <input type="submit" value="filter" name="filterByYear">
         </form>
         <table id="customers">
             <tr>
-            <th>Months</th>
-              <th>Meter Box</th>
-              <th>Beggining Meter Reading</th>
-              <th>End Meter Reading</th>
-              <th>Unit Consumed</th>
-              <th>Price</th>
-              <th>Date</th>
+                <th>Month</th>
+                <th>Meter Box Number</th>
+                <th>Beggining Meter Reading</th>
+                <th>End Meter Reading</th>
+                <th>Unit Consumed</th>
+                <th>Price</th>
+                <th>Date</th>
             </tr>
-            <tr >
-            <td>january</td>
-              <td>12364768912</td>
-              <td>3884</td>
-              <td>4884</td>
-              <td>4884</td>
-              <td>400ksh</td>
-              <td>2020-02-05</td>
-            </tr>
-            <tr>
-            <td>february</td>
-              <td>213456789123</td>
-              <td>4884</td>
-              <td>5887</td>
-              <td>5887</td>
-              <td>200ksh</td>
-              <td>11-01-2011</td>
-            </tr>
-            <tr>
-            <td>march</td>
-              <td>13456778912</td>
-              <td>5887</td>
-              <td>6888</td>
-              <td>6888</td>
-              <td>500ksh</td>
-              <td>2020-10-10</td>
-            </tr>
+            <?php
+                $query1     = "SELECT * FROM consumption WHERE YEAR(createdAt) = '$theYear'";
+                $results 	= mysqli_query($con, $query1);
+                echo $theYear;
+                while($row = $results->fetch_assoc()) {
+                    // GET PRODUCT CATEGORY NAME
+                    $meterBoxId = $row['meterBoxId'];
+                    $query2   = "SELECT * FROM meterbox WHERE meterBoxId = '$meterBoxId'";
+                    $result2 = mysqli_query($con, $query2);
+                    if (mysqli_num_rows($result2) == 1) {
+                        $meterBoxData = $result2->fetch_assoc();		
+                    }
+                    ?>
+                        <tr>
+                            <td><?php echo date('M',strtotime($row['createdAt'])) ?></td>
+                            <td><?php echo $meterBoxData['meterBoxNumber'] ?></td>
+                            <td>5887</td>
+                            <td>6888</td>
+                            <td>6888</td>
+                            <td>500ksh</td>
+                            <td><?php echo date('M d Y',strtotime($row['createdAt'])) ?></td>
+                        </tr>
+                    <?php                    
+                }
+            ?>
+
+            
           </table>
 
           
