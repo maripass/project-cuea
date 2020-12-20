96821879984ac2858d263a4d50117d6a5193a7f9
Maman mary2 <azzizasolmemvictoria@gmail.com>
2020-12-20 12:19:58 +0300

    --- Auto-Git Commit ---

--- annually-report.php
+++ annually-report.php
@@ -8,7 +8,6 @@
 	$theYear   = date("Y");
 	if (isset($_POST['filterByYear'])) {
         $yearInput = mysqli_real_escape_string($con, $_POST['yearInput']);
-        echo $_POST['yearInput'];
 		$theYear = $yearInput;
 	}
   
@@ -39,14 +38,14 @@
 
         <form id="filter" style="margin-top:15px; float:right;right:0px;margin-right:30px;" method="POST">
           <input type="number" name="yearInput" id="yearInput" style="padding:10px; width:100%" max="2020" min="2016"placeholder="YYYY" value="<?php echo $yearInput ?>">
-          <input type="submit" value="filter" name="filterByYear">
+          <input type="submit" value="Filter" name="filterByYear">
         </form>
         <table id="customers">
             <tr>
                 <th>Month</th>
                 <th>Meter Box Number</th>
-                <th>Beggining Meter Reading</th>
-                <th>End Meter Reading</th>
+                <th>Previous Month Meter Reading</th>
+                <th>Current Month Meter Reading</th>
                 <th>Unit Consumed</th>
                 <th>Price</th>
                 <th>Date</th>
@@ -54,7 +53,6 @@
             <?php
                 $query1     = "SELECT * FROM consumption WHERE YEAR(createdAt) = '$theYear'";
                 $results 	= mysqli_query($con, $query1);
-                echo $theYear;
                 while($row = $results->fetch_assoc()) {
                     // GET PRODUCT CATEGORY NAME
                     $meterBoxId = $row['meterBoxId'];
@@ -63,13 +61,18 @@
                     if (mysqli_num_rows($result2) == 1) {
                         $meterBoxData = $result2->fetch_assoc();		
                     }
+                    if($row['previoustMeterReading'] < $row['currentMeterReading']) {
+                        $unitConsummed = $row['currentMeterReading'];
+                    } else {
+                        $unitConsummed = $row['previoustMeterReading'] - $row['currentMeterReading'];
+                    }
                     ?>
                         <tr>
                             <td><?php echo date('M',strtotime($row['createdAt'])) ?></td>
                             <td><?php echo $meterBoxData['meterBoxNumber'] ?></td>
-                            <td>5887</td>
-                            <td>6888</td>
-                            <td>6888</td>
+                            <td><?php echo $row['previoustMeterReading'] ?></td>
+                            <td><?php echo $row['currentMeterReading'] ?></td>
+                            <td><?php echo $unitConsummed ?></td>
                             <td>500ksh</td>
                             <td><?php echo date('M d Y',strtotime($row['createdAt'])) ?></td>
                         </tr>
